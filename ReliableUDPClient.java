2ecdad57d870a338954d419186ac468789455495
jboj <jboj@itu.dk>
2020-09-21 12:54:27 +0200

    Co-authored-by: chfo@itu.dk <chfo@itu.dk> Co-authored-by: mirb@itu.dk <mirb@itu.dk> Co-authored-by: mmau@itu.dk <mmau@itu.dk> Co-authored-by: jpor@itu.dk <jpor@itu.dk>

--- ReliableUDPClient.java
+++ ReliableUDPClient.java
@@ -1,3 +1,54 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.UUID;
+
 public class ReliableUDPClient {
-    
+    public static final int CHECK_LENGTH = 36;
+
+    private InetAddress toAddress;
+    private int toPort;
+    private String message;
+    private DatagramSocket socket;
+
+    public static void main(String[] args) throws IOException{
+        new ReliableUDPClient(InetAddress.getByName(args[0]), Integer.parseInt(args[1]), args[2]);
+        
+    }
+
+    public ReliableUDPClient (InetAddress toAddress, int toPort, String message) throws IOException{
+        this.toAddress = toAddress;
+        this.toPort = toPort;
+        this.message = message;
+
+        socket = new DatagramSocket(8008);
+        socket.setSoTimeout(250);
+        send();
+        socket.close();
+    }
+
+    private void send()throws IOException{
+        String generatedUuid = UUID.randomUUID().toString();
+        String receivedUuid = "";
+
+        while(!generatedUuid.equals(receivedUuid)){
+            var messageBytes = (generatedUuid + message).getBytes();
+            DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, toAddress, toPort);
+            socket.send(packet);
+
+            receivedUuid = receiveCheck().trim();
+        //    System.out.println(receivedUuid + " =? " + generatedUuid);
+          //  System.out.println(receivedUuid.equals(generatedUuid));
+        }   
+    }
+
+    private String receiveCheck()throws IOException{
+        try {
+            DatagramPacket check = new DatagramPacket(new byte[CHECK_LENGTH], CHECK_LENGTH);
+            socket.receive(check);
+            return new String(check.getData());
+        } catch (SocketTimeoutException e) {
+            // Received no message, return empty checksum
+            return "";
+        }
+    }
 }
--- ReliableUDPServer.java
+++ ReliableUDPServer.java
@@ -1,3 +1,48 @@
+import java.net.*;
+import java.util.LinkedList;
+
 public class ReliableUDPServer {
-    
+    private static LinkedList<String> codes = new LinkedList<String>();
+    public static void main(String[] args){
+        DatagramSocket aSocket = null;
+        try{
+            aSocket = new DatagramSocket(7007);
+        } catch (Exception e){
+            e.printStackTrace();
+            aSocket.close();
+            return;
+        }
+
+        try{
+            while(true){
+                //Receive reply
+                try{
+                    byte[] recieved = new byte[291];// Allocate a buffer into which the code is written 36b code and 255b msg
+                    
+                    DatagramPacket formated = new DatagramPacket(recieved, recieved.length);
+                    aSocket.receive(formated);
+                    DatagramPacket code = new DatagramPacket(formated.getData(), ReliableUDPClient.CHECK_LENGTH, formated.getAddress(), formated.getPort());
+                    String msg = new String(formated.getData()).substring(ReliableUDPClient.CHECK_LENGTH);
+                    
+                    
+                    if(!codes.contains(new String(code.getData()))){
+                        System.out.println(msg);
+                        if(codes.size() > 64) codes.removeFirst();
+                        codes.add(new String(code.getData()));
+                    }
+                    
+                    aSocket.send(code);
+                    
+        
+                } catch(Exception e){
+                    e.printStackTrace();
+                    aSocket.close();
+                    return;
+                }
+            }
+        }
+        finally{
+            aSocket.close();
+        }
+    }
 }
